27034df2645a336f1e629c1aab8966fe96124ca5
Reenku Choudhary <105987075+Reenku19@users.noreply.github.com>
2024-09-15 23:58:22 +0530

    Add files via upload

---
+++ program.py
@@ -0,0 +1,27 @@
+
+
+print("Hello")
+print("My Name is Reenku choudhary")
+
+
+a=int(input("Enter the a value"))
+b=int(input("Enter the b value"))
+print("sum of both values:", a+b)
+
+
+a=int(input("Enter the a value"))
+b=int(input("Enter the b value"))
+print("quotient is:",a/b)
+
+
+a=int(input("Enter the a value"))
+b=int(input("Enter the b value"))
+print("product of both values:",a*b)
+
+
+a=int(input("Enter the a value"))
+b=int(input("Enter the b value"))
+c=int(input("Enter the c value"))
+h=int(input("Enter the h value"))
+print("Area is: ",0.5*b*h)
+print("Perimeter is:",a+b+c)
